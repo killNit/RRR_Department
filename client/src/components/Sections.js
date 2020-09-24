c21934090e5f851ee88042d6948df9826621cfff
killNit <rileysanti@gmail.com>
2020-09-24 04:52:45 -0600

    seeded 10 departments

--- client/src/components/Sections.js
+++ client/src/components/Sections.js
@@ -8,6 +8,7 @@ const Sections = () => {
   const [sections, setSections] = useState([]);
 
   useEffect( () => {
+    debugger;
     axios.get("/api/sections")
     .then((res) => {
       setSections(res.data);
